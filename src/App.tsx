d35697187b9a2b10fd0726854eb9f4daeb9d5481
Sahil Metalwala <homefolder@Sahils-MacBook-Pro.local>
2024-04-04 23:14:40 -0500

    feat: add auth firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+  import { useContext, useEffect } from 'react'
+  import { Routes , Route, useNavigate } from 'react-router-dom' 
+  import { AuthContext } from './context/auth-context'
+  import RequireAuth from './components/require-auth'
+  import Home from './routes/home'
+  import Profile from './routes/profile'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  function App() {
+    const { currentUser } = useContext(AuthContext)
+    const navigate = useNavigate()
 
-export default App
+    // NOTE: console log for testing purposes
+    console.log('User:', !!currentUser);
+
+    // Check if currentUser exists on initial render
+    useEffect(() => {
+      if (currentUser) {
+        navigate('/profile')
+      }
+    }, [currentUser, navigate])
+      
+    return (
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="profile" element={
+          <RequireAuth>
+            <Profile />
+          </RequireAuth>}
+        />
+      </Routes>
+    )
+  }
+
+  export default App
+  
---
+++ src/firebase/firebase-config.ts
@@ -0,0 +1,20 @@
+
+  const config = {
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
+
+  export function getFirebaseConfig() {
+    if (!config || !config.apiKey) {
+      throw new Error('No Firebase configuration object provided.' + '\n' +
+      'Add your web app\'s configuration object to firebase-config.ts');
+    } else {
+      return config;
+    }
+  }    
+  
